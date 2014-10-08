b038e51ccf2fb3f3313972d25b739dc229f164e6
Tom Pullen <tompullen@toms-mbp.lan>
2014-10-08 21:39:10 +0100

    Initial Commit

---
+++ OuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Tom Pullen on 07/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
