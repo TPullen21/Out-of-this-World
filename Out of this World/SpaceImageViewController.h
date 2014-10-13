4d23c39f2b7927606311b416193c6f88601d6780
Tom Pullen <tpullen21@gmail.com>
2014-10-13 20:47:55 +0100

    Added a new view controller

---
+++ Out of this World/SpaceImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpaceImageViewController.h
+//  Out of this World
+//
+//  Created by Tom Pullen on 12/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceImageViewController : UIViewController
+
+@end
---
+++ Out of this World/SpaceImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  SpaceImageViewController.m
+//  Out of this World
+//
+//  Created by Tom Pullen on 12/10/2014.
+//
+//
+
+#import "SpaceImageViewController.h"
+
+@interface SpaceImageViewController ()
+
+@end
+
+@implementation SpaceImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
