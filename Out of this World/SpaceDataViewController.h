d9cc1e552e3b4dff8cb3553e62888e25fa579ad3
Tom Pullen <tpullen21@gmail.com>
2014-10-14 22:14:30 +0100

    Added a data view controller

---
+++ Out of this World/SpaceDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpaceDataViewController.h
+//  Out of this World
+//
+//  Created by Tom Pullen on 14/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceDataViewController : UIViewController
+
+@end
