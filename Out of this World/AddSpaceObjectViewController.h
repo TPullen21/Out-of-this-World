cf1b7c6314284fdc4f7b1c069b21c13f47bb5229
Tom Pullen <tpullen21@gmail.com>
2014-10-20 21:24:48 +0100

    Added 'add' view controller

---
+++ Out of this World/AddSpaceObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Tom Pullen on 20/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSpaceObjectViewController : UIViewController
+
+@end
---
+++ Out of this World/AddSpaceObjectViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddSpaceObjectViewController.m
+//  Out of this World
+//
+//  Created by Tom Pullen on 20/10/2014.
+//
+//
+
+#import "AddSpaceObjectViewController.h"
+
+@interface AddSpaceObjectViewController ()
+
+@end
+
+@implementation AddSpaceObjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
